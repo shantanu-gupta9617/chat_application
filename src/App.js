a7ab3f5e0c4ca24772009814583b819a46d43f15
shantanu-gupta9617 <shantanu9617@gmail.com>
2021-06-11 16:49:58 +0530

    add projectid,username,secretkey manually

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+return(
+    <ChatEngine 
+    height="100vh"
+    projectID="619e87c2-41c0-4073-be55-ba68b06426f3"
+    username="javascriptmastery123123"
+    userSecret="123123"
+    />
+    );
 }
-
 export default App;
